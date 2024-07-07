377c4e0560c99e7ae346857b105d365257999bf8
Yubal <ydefuente@gmail.com>
2024-07-07 02:01:50 +0200

    pundo antes de instalar framer-motion

--- src/components/GenericModal/GenericModal.jsx
+++ src/components/GenericModal/GenericModal.jsx
@@ -1,24 +1,8 @@
 import "./GenericModal.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
-import { useEffect } from "react";
 
 const GenericModal = ({ isOpen, onClose, children }) => {
-
-    // Logica para cerrar el modal al hacer click fuera de él
-
-    // useEffect(() => {
-    //     const handleOutsideClick = (event) => {
-    //         if (event.target.classList.contains("modal-overlay")) {
-    //             onClose();
-    //         }
-    //     };
-
-    //     document.addEventListener("click", handleOutsideClick);
-    //     return () => {
-    //         document.removeEventListener("click", handleOutsideClick);
-    //     };
-    // }, [onClose]);
 
     if (!isOpen) return null;
 
